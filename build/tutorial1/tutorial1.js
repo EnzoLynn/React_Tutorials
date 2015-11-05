37803854e34c7ebe64e288a586364cecbde45e30
chenheling <6710582@qq.com>
2015-11-05 10:58:39 +0800

    product

--- build/tutorial1/tutorial1.js
+++ build/tutorial1/tutorial1.js
@@ -11,7 +11,49 @@ var CommentBox = React.createClass({
 		);
 	}
 });
+var MyComponent = React.createClass({
+	displayName: "MyComponent",
+
+	handleClick: function handleClick() {
+		// Explicitly focus the text input using the raw DOM API.
+		this.refs['myTextInput'].focus();
+	},
+	render: function render() {
+		// The ref attribute adds a reference to the component to
+		// this.refs when the component is mounted.
+		return React.createElement(
+			"div",
+			null,
+			React.createElement("input", { type: "text", ref: "myTextInput" }),
+			React.createElement("input", {
+				type: "button",
+				value: "Focus the text input",
+				onClick: this.handleClick
+			})
+		);
+	}
+});
 
 $(function () {
 	ReactDOM.render(React.createElement(CommentBox, null), $('#content')[0]);
+
+	ReactDOM.render(React.createElement(MyComponent, null), document.getElementById('example'));
+
+	(function () {
+		console.log('Welcome to the Internet.');
+	})();
+
+	var a = 'foobar';
+	var b = "foo" + a + "bar";
+	var c = 'foobar';
+
+	console.log(b);
+
+	var arr = [1, 2, 3, 4];
+
+	// good
+	var first = arr[0];
+	var second = arr[1];
+
+	console.log(first);
 });
--- src/tutorial1/tutorial1.js
+++ src/tutorial1/tutorial1.js
@@ -7,11 +7,57 @@ var CommentBox = React.createClass({
 		);
 	}
 });
+var MyComponent = React.createClass({
+	handleClick: function() {
+		// Explicitly focus the text input using the raw DOM API. 
+		this.refs['myTextInput'].focus();
+	},
+	render: function() {
+		// The ref attribute adds a reference to the component to
+		// this.refs when the component is mounted.
+		return (
+			<div>
+        <input type="text" ref="myTextInput" />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.handleClick}
+        />
+      </div>
+		);
+	}
+});
 
 
-$(function() { 
+
+$(function() {
 	ReactDOM.render(
 		<CommentBox />,
 		$('#content')[0]
 	);
+
+	ReactDOM.render(
+		<MyComponent />,
+		document.getElementById('example')
+	);
+
+	(() => {
+		console.log('Welcome to the Internet.');
+	})();
+ 
+	const a  = 'foobar';
+	const b = `foo${a}bar`;
+	const c = 'foobar';
+
+	console.log(b);
+
+	const arr = [1, 2, 3, 4];
+ 
+
+	// good
+	const [first, second] = arr;
+	console.log(first);
+ 
+
+
 });
