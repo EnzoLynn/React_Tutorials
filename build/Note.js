f023b386b9ad0e75940293a35defbe14c36c76b1
chenheling <6710582@qq.com>
2015-11-09 16:02:51 +0800

    Notes

--- build/Note.js
+++ build/Note.js
@@ -1,19 +1,32 @@
 'use strict';
 
 define(function (require, exports, module) {
+	var $ = require('jquery.min.js');
 	var dbHelper = require('build/WebSqlHelper');
 	dbHelper.openDatabase();
 
-	dbHelper.select('LOGS', '*', {
-		"id": 2
+	dbHelper.createTable('Notes', {
+		id: "integer primary key autoincrement",
+		title: "not null",
+		content: ""
 	}, function (message) {
 		console.log(message);
-		if (message.success) {
-			for (var i = 0; i < message.result.rows.length; i++) {
-				console.log(message.result.rows[i]);
-			};
-		};
 	});
+
+	// dbHelper.select('LOGS', '*', {
+	// 	"id": 2
+	// }, function(message) {
+	// 	console.log(message);
+	// 	if (message.success) {
+	// 		for (var i = 0; i < message.result.rows.length; i++) {
+	// 			console.log(message.result.rows[i]);
+	// 		};
+	// 	};
+	// });
+
+	// dbHelper.dropTable('test', function(message) {
+	// 	console.log(message);		
+	// });
 	// dbHelper.update('LOGS',{log:'update'},{
 	// 	id:2
 	// }, function(message) {
@@ -24,10 +37,109 @@ define(function (require, exports, module) {
 	// 	 console.log(message);
 	// });
 
-	// dbHelper.insert('LOGS',{
-	// 	id:3,
-	// 	log:(new Date()).getTime()
+	// dbHelper.insert('Notes',{	
+
+	// 	title:(new Date()).getTime(),
+	// 	content:'tes1t'
 	// },function(message){
 	// 	console.log(message);
 	// });
+	//
+	//
+
+	var NoteRows = React.createClass({
+		displayName: 'NoteRows',
+
+		render: function render() {
+			return React.createElement(
+				'tr',
+				{ content: this.props.note.content },
+				React.createElement(
+					'td',
+					{ className: 'NoteRow' },
+					this.props.note.id
+				),
+				React.createElement(
+					'td',
+					{ className: 'NoteRow' },
+					this.props.note.title
+				)
+			);
+		}
+	});
+	var NoteHead = React.createClass({
+		displayName: 'NoteHead',
+
+		addNote: function addNote() {},
+		render: function render() {
+			return React.createElement(
+				'tr',
+				{ className: '' },
+				React.createElement(
+					'th',
+					{ colSpan: '2', className: 'text-center' },
+					'备忘录',
+					React.createElement(
+						'span',
+						{ style: { float: "right" } },
+						React.createElement('input', { className: 'btn btn-default', type: 'button', value: '+', onclick: this.addNote })
+					)
+				)
+			);
+		}
+	});
+
+	var NoteList = React.createClass({
+		displayName: 'NoteList',
+
+		getInitialState: function getInitialState() {
+			return {
+				notes: []
+			};
+		},
+		loadNoteFromWebsql: function loadNoteFromWebsql() {
+			var me = this;
+			dbHelper.select('Notes', '*', false, function (message) {
+				console.log(message);
+				if (message.success) {
+					var arr = [];
+
+					for (var i = 0; i < message.result.rows.length; i++) {
+						arr.push(message.result.rows[i]);
+					};
+					me.setState({
+						notes: arr
+					});
+				};
+			});
+		},
+		componentDidMount: function componentDidMount() {
+			this.loadNoteFromWebsql();
+		},
+		render: function render() {
+			var rows = [];
+			console.log(this.state.notes);
+			this.state.notes.forEach(function (note, key) {
+				rows.push(React.createElement(NoteRows, { key: key, note: note }));
+			});
+			return React.createElement(
+				'table',
+				{ className: 'NoteList table table-hover' },
+				React.createElement(
+					'thead',
+					null,
+					React.createElement(NoteHead, null)
+				),
+				React.createElement(
+					'tbody',
+					null,
+					rows
+				)
+			);
+		}
+	});
+
+	$(function () {
+		ReactDOM.render(React.createElement(NoteList, null), $('.content').get(0));
+	});
 });
--- src/Note.js
+++ src/Note.js
@@ -1,18 +1,31 @@
 define(function(require, exports, module) {
+	var $ = require('jquery.min.js');
 	var dbHelper = require('build/WebSqlHelper');
 	dbHelper.openDatabase();
 	 
- 
-	dbHelper.select('LOGS', '*', {
-		"id": 2
+	dbHelper.createTable('Notes', {
+		id: "integer primary key autoincrement",
+		title: "not null",
+		content: ""
 	}, function(message) {
 		console.log(message);
-		if (message.success) {
-			for (var i = 0; i < message.result.rows.length; i++) {
-				console.log(message.result.rows[i]);
-			};
-		};
 	});
+
+
+	// dbHelper.select('LOGS', '*', {
+	// 	"id": 2
+	// }, function(message) {
+	// 	console.log(message);
+	// 	if (message.success) {
+	// 		for (var i = 0; i < message.result.rows.length; i++) {
+	// 			console.log(message.result.rows[i]);
+	// 		};
+	// 	};
+	// });
+
+	// dbHelper.dropTable('test', function(message) {
+	// 	console.log(message);		 
+	// });
 	// dbHelper.update('LOGS',{log:'update'},{
 	// 	id:2
 	// }, function(message) {
@@ -23,10 +36,93 @@ define(function(require, exports, module) {
 	// 	 console.log(message);
 	// });
 
-	// dbHelper.insert('LOGS',{
-	// 	id:3,
-	// 	log:(new Date()).getTime()
+	// dbHelper.insert('Notes',{	
+
+	// 	title:(new Date()).getTime(),
+	// 	content:'tes1t'
 	// },function(message){
 	// 	console.log(message);
 	// });
+	// 
+	// 
+
+
+	let NoteRows = React.createClass({
+		render: function() {
+			return (
+				<tr content={this.props.note.content}>
+					<td className='NoteRow'>{this.props.note.id}</td>
+					<td className='NoteRow'>{this.props.note.title}</td> 
+				</tr>
+			);
+		}
+	});
+	let NoteHead = React.createClass({
+		addNote: function() {
+
+		},
+		render: function() {
+			return (
+				<tr className=''>
+				<th colSpan='2' className='text-center'>
+					备忘录
+					<span style={{float:"right"}}>
+					<input className='btn btn-default' type="button" value='+' onclick={this.addNote}/>
+					</span>
+				</th>
+				</tr>
+			);
+		}
+	});
+
+	let NoteList = React.createClass({
+		getInitialState: function() {
+			return {
+				notes: []
+			};
+		},
+		loadNoteFromWebsql: function() {
+			var me = this;
+			dbHelper.select('Notes', '*', false, function(message) {
+				console.log(message);
+				if (message.success) {
+					var arr = [];
+					
+					for (var i = 0; i < message.result.rows.length; i++) {
+						arr.push(message.result.rows[i]);
+					};
+					me.setState({
+						notes:arr
+					});
+				};
+			});
+		},
+		componentDidMount: function() {
+			this.loadNoteFromWebsql();
+		},
+		render: function() {
+			var rows = [];
+			console.log(this.state.notes);
+			this.state.notes.forEach(function(note, key) {
+				rows.push(<NoteRows key={key} note={note}/>);
+			});
+			return (
+				<table className='NoteList table table-hover'>
+						<thead><NoteHead/></thead>
+						<tbody>
+						{rows}
+						</tbody>
+				</table>
+			);
+
+		}
+	});
+	 
+	$(function() {
+		ReactDOM.render(
+			<NoteList />,
+			$('.content').get(0)
+		);
+	});
+
 });
